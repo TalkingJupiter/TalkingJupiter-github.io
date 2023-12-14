54bc94636eb7d82342778414e162b6a505540afe
Batuhan Sencer <dev.batuhansencer@gmail.com>
2023-12-14 08:58:51 -0600

    Scroll Funtions

--- script.js
+++ script.js
@@ -14,8 +14,23 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add('active-tab');
 }
 
-// Responsive side navbar
 
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+// Responsive side navbar
 
 function openmenu(){
     sidemenu.style.right = '0';
