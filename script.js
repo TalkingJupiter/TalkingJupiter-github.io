440410125509f546991369f0f8db93e7952e3374
Batuhan Sencer <dev.batuhansencer@gmail.com>
2023-12-13 19:30:28 -0600

    Submiting the message from contact me form

--- script.js
+++ script.js
@@ -13,8 +13,22 @@ function opentab(tabname){
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
-
-
+// Contact Me
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxtlTp5v-kBBbxZdaOs2mVAcnS4foezXeY9bFxP2_TcQBiDDQfkxBCU0t8yM7ANZpA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = 'Message sent succesfully'
+        setTimeout(function(){
+            msg.innerHTML = ''
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
 
 // Responsive side navbar
 
